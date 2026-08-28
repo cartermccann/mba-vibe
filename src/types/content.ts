0615716d8d2e7c782bf31b5216f2d818fa9408a4
CARTER MCCANN <carter@comcreate.io>
2026-08-27 18:57:32 -0600

    Add utils, content types, and tsconfig

---
+++ src/types/content.ts
@@ -0,0 +1,18 @@
+export type StatItem = {
+  value: string
+  textBefore?: string
+  emphasis?: string
+  textAfter?: string
+  label?: string
+}
+
+export type FeatureColumn = {
+  title: string
+  description?: string
+}
+
+export type ProtocolStep = {
+  number: string
+  title: string
+  description: string
+}
